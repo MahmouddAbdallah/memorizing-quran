be7d6df387ffd8d9806de0f530bd065d1fd0fd70
mahmoudmohamed <mmaahm2002@gmail.com>
2024-04-26 18:50:32 +0300

    Move `getPlans` function to a separate server component file and update the `SubscribePlan` component to use the new function. Wrap the function call in a try-catch block to handle any errors.

--- app/utils/get-data/fetchPlan.ts
+++ app/utils/get-data/fetchPlan.ts
@@ -1,5 +1,17 @@
 'use server';
 import { cookies } from "next/headers";
+export async function getPlans() {
+    const res = await fetch(`${process.env.BASE_URL}/api/plan`, {
+        method: "GET",
+        credentials: 'include',
+        next: { revalidate: 100 }
+    })
+
+    if (!res.ok) {
+        throw new Error('Failed to fetch data')
+    }
+    return res.json()
+}
 
 export async function getPlan(id: string) {
     const token = await cookies().get('token')?.value
