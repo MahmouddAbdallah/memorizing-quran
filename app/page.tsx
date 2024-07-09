903ac44b8abd0f273b325b72beb130c3d067dcef
mahmoudmohamed <mmaahm2002@gmail.com>
2024-07-09 18:28:30 +0300

    "Added token validation and redirects to home page if user is authenticated in sign-in and sign-up pages, and updated home page to redirect to profile/schedule if user is not an admin"

--- app/page.tsx
+++ app/page.tsx
@@ -12,8 +12,10 @@ export const dynamic = 'force-dynamic'
 const Home = () => {
   const token = cookies().get('token')?.value;
   const user = getRole(token)
-  if (user?.role != 'admin') {
-    redirect('/profile/schedule')
+  if (token) {
+    if (user?.role != 'admin') {
+      redirect('/profile/schedule')
+    }
   }
   return (
     <div>
--- app/sign-up/page.tsx
+++ app/sign-up/page.tsx
@@ -1,7 +1,15 @@
 import React from 'react'
 import FormData from './components/FormData'
+import { getRole } from '@/lib/verfiyAuth';
+import { cookies } from 'next/headers';
+import { redirect } from 'next/navigation';
 
 const page = () => {
+    const token = cookies().get('token')?.value;
+    const user = getRole(token)
+    if (user) {
+        redirect('/')
+    }
     return (
         <div className=' pb-20'>
             <div className='bg-primary pt-16 pb-40 text-center'>
